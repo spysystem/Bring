af2f66d3b406b736d19eeb7e70c91d08c6201654
Thomas Hansen <thomas@thomasphansen.dk>
2018-11-23 11:30:28 +0100

    Fixed issues with error messages return types

--- src/messages.php
+++ src/messages.php
@@ -9,31 +9,31 @@ class messages
 {
 
 	/**
-	 * @var MessageResponseType[] $message
+	 * @var MessageResponseType[]|MessageResponseType|null $message
 	 */
 	protected $message;
 
 	/**
-	 * @param MessageResponseType[] $message
+	 * @param MessageResponseType[]|MessageResponseType|null $message
 	 */
-	public function __construct(array $message)
+	public function __construct($message)
 	{
 		$this->message = $message;
 	}
 
 	/**
-	 * @return MessageResponseType[]
+	 * @return MessageResponseType[]|MessageResponseType|null
 	 */
-	public function getMessage(): ?array
+	public function getMessage()
 	{
 		return $this->message;
 	}
 
 	/**
-	 * @param MessageResponseType[] $message
+	 * @param MessageResponseType[]|MessageResponseType|null $message
 	 * @return messages
 	 */
-	public function setMessage(array $message): messages
+	public function setMessage($message): messages
 	{
 		$this->message = $message;
 		return $this;
